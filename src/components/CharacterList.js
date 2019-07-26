de53a30b8aad70b421c45271ae73edc2ce3219ac
tatiana faramarzi <tfaramarzi@gmail.com>
2019-07-26 08:51:05 -0700

    Added axios call in useEffect hook to fetch data from API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState()
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getCharacters = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+          console.log(response.data.results)
+          setCharacters(response.data.results);
+        })
+        .catch(error => {
+          console.error('There was a server error', error);
+        });
+    }
+
+    getCharacters();
   }, [])
 
-  return <section className='character-list grid-view'>
-
+  return (
+    <section className='character-list grid-view'>
       <h2>TODO: `array.map()` over your state here!</h2>
     </section>
+
+  )
+
+
 
 }
